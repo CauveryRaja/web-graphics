0b88568579c53c680083503a04c5e22d14049e80
CauveryRaja <cauveryraja25@gmail.com>
2021-06-12 09:06:37 +0530

    Introduce new Snake construction and animation logic

--- canvas/snake-game/src/app.js
+++ canvas/snake-game/src/app.js
@@ -7,7 +7,23 @@ canvasWidth = canvasElm.offsetWidth;
 canvasHeight = canvasElm.offsetHeight;
 
 const canvas = new Canvas(canvasElm);
-const snake = new Snake(canvasWidth/3, canvasHeight/2);
+const snake = new Snake(canvasWidth/2, canvasHeight/2);
 
 canvas.paint(snake.draw.bind(snake));
 canvas.animate(snake.move.bind(snake));
+
+document.body.addEventListener('keydown', function(event) {
+    // Works for 'keydown', 'keypress' does not detect arrow keys
+    // switch(event.key) {
+    //     case 'ArrowRight': snakeObj['direction'] = 'right';
+    //                         break;
+    //     case 'ArrowUp': snakeObj['direction'] = 'up';
+    //                         break;
+    //     case 'ArrowLeft': snakeObj['direction'] = 'left';
+    //                         break;
+    //     case 'ArrowDown': snakeObj['direction'] = 'down';
+    //                         break;
+    // }
+    let direction = event.key.split('Arrow')[1];
+    snake.changeDirection(direction.toLowerCase());
+})
--- canvas/snake-game/src/snake/Snake.js
+++ canvas/snake-game/src/snake/Snake.js
@@ -1,25 +1,27 @@
 class Snake {
     constructor(x, y) {
-        this.head = {};
-        this.tail = {x, y};
+        // this.head = {};
+        // this.tail = {x, y};
+
+        this.position = [{x,y}];
         this.direction = 'right';
     }
 
-    draw(cxt) {
-        cxt.fillRect(this.tail.x, this.tail.y, 200, 10);
-        this.head = {
-            x: this.tail.x + 200,
-            y: this.tail.y
-        }
-    }
+    // draw(cxt) {
+    //     cxt.fillRect(this.tail.x, this.tail.y, 200, 10);
+    //     this.head = {
+    //         x: this.tail.x + 200,
+    //         y: this.tail.y
+    //     }
+    // }
 
-    drawSnake(cxt, x, y) {
-        this.position = [{x,y}]; // head position
-        cxt.fillRect(x, y, 10, 10);
-        for(let i=1; i<=5; i++) {
+    draw(cxt) {
+        // this.position = [{x,y}]; // head position
+        cxt.fillRect(this.position[0].x, this.position[0].y, 10, 10);
+        for(let i=1; i<=9; i++) {
             let pos = {
                 x: this.position[i-1].x - 10, // right moving snake. so, pos - head
-                y: y
+                y: this.position[i-1].y
             };
             this.position.push(pos);
             cxt.fillRect(pos.x, pos.y, 10, 10);
@@ -27,18 +29,38 @@ class Snake {
     }
 
     move(cxt) {
-        cxt.fillRect(this.head.x, this.head.y, 10, 10);
-        cxt.clearRect(this.tail.x, this.tail.y, 10, 10);
-        this.updateHeadTail();
+        // Compute new head position
+        this.computeHeadPosition();
+        cxt.fillRect(this.position[0].x, this.position[0].y, 10, 10);
+        let tail = this.position.pop();
+        cxt.clearRect(tail.x, tail.y, 10, 10);
+        console.log(JSON.stringify(this.position));
     }
 
-    updateHeadTail() {
-        console.log('Updating head, tail ...', this);
-        // this.head.x += 10; 
-        this.head.x += 10; // For direction right
+    changeDirection(direction) {
+        if(direction === 'right' && this.direction !== 'left')
+            this.direction = 'right';
+        else if(direction === 'left' && this.direction !== 'right')
+            this.direction = 'left';
+        else if(direction === 'top' && this.direction !== 'bottom')
+            this.direction = 'top';
+        else if(direction === 'bottom' && this.direction !== 'top')
+            this.direction = 'bottom';
+    }
 
-        // this.tail.x += 10;
-        this.tail.x += 10; // For direction right
+    computeHeadPosition() {
+        let newHead = Object.assign({}, this.position[0]);
+        switch(this.direction) {
+            case 'left': newHead.x -= 10;
+                            break;
+            case 'right': newHead.x += 10;
+                            break;
+            case 'top': newHead.y -= 10;
+                            break;
+            case 'bottom': newHead.y += 10;
+                            break;
+        }
+        this.position.unshift(newHead);
     }
 }
 
