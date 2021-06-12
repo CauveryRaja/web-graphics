b1a9ceb66efd01b4e31accd702661237b5b7b66f
CauveryRaja <cauveryraja25@gmail.com>
2021-06-12 18:11:30 +0530

    Remove Canvas class and move animation loop to app

--- canvas/snake-game/src/app.js
+++ canvas/snake-game/src/app.js
@@ -1,16 +1,18 @@
-import Canvas from './canvas/Canvas';
 import Snake from './snake/Snake';
 
 const canvasElm = document.getElementById('canvas');
-let canvasWidth, canvasHeight;
+let canvasWidth, canvasHeight, cxt;
 canvasWidth = canvasElm.offsetWidth;
 canvasHeight = canvasElm.offsetHeight;
+cxt = canvasElm.getContext('2d');
 
-const canvas = new Canvas(canvasElm);
 const snake = new Snake(canvasWidth/2, canvasHeight/2);
 
-canvas.paint(snake.draw.bind(snake));
-canvas.animate(snake.move.bind(snake));
+snake.draw(cxt);
+
+setInterval(() => {
+    snake.move(cxt);
+}, 100);
 
 document.body.addEventListener('keydown', function(event) {
     let direction = event.key.split('Arrow')[1];
