4908c4a40aa150485572185ff1a6d9a22c8c62cb
CauveryRaja <cauveryraja25@gmail.com>
2021-07-14 20:26:21 +0530

    Create new JS app for Snake game

---
+++ canvas/snake-game-routine/v1/src/app.js
@@ -0,0 +1,12 @@
+const canvasElm = document.getElementById('canvas');
+const cxt = canvasElm.getContext('2d');
+
+if(!cxt) {
+    throw 'Canvas context not available!'
+}
+
+// Draw snake
+
+// Animate snake
+
+// Add Keyboard Controls
